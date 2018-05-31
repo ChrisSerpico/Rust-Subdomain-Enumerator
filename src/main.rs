5f2eb25f74e00724f3d453f743b5ad039a74692a
vkili <vkili@users.noreply.github.com>
2018-05-31 15:54:18 -0500

    modify main to use libs

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,16 @@ extern crate subdomain_enumerator;
 extern crate clap;
 
 use clap::{Arg, App};
-// used to read arguments passed on command line
-
 use std::collections::HashSet;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use subdomain_enumerator::query;
-use subdomain_enumerator::library_enumerator;
+use subdomain_enumerator::results;
 
 
 
 fn main() {
     let mut config = Config::new();
-    // read arguments from command line 
     let matches = App::new("Concurrent Subdomain Enumerator")
                           .version("1.0")
                           .about("Queries VirusTotal for subdomains and performs dictionary enumeration.")
@@ -32,36 +29,33 @@ fn main() {
                                .help("Specifies the wordlist to use for dictionary enumeration."))
                           .get_matches();
 
-    config.domains = matches.values_of_lossy("domains").unwrap();
-    config.num_domains = config.domains.len();
-    let subdomains : Vec<Arc<Mutex<HashSet<String>>>>  = vec![Arc::new(Mutex::new(HashSet::new())); config.num_domains];
+    config.add_domains(matches.values_of_lossy("domains").unwrap());
+    let results = Results::new(config.get_num_domains());
+
     if matches.is_present("limit") {
         let limit_arg = matches.value_of("limit").unwrap();
-        config.limit = limit_arg.parse().unwrap();
+        config.set_limit(limit_arg.parse().unwrap());
     }  
+
     let mut threads = Vec::new();
 
     if matches.is_present("wordlist") {
-        config.library = matches.value_of("wordlist").unwrap().to_string();
+        config.set_library(matches.value_of("wordlist").unwrap().to_string());
 
-        for i in 0..config.num_domains {
-            let domain = config.domains[i].to_string();
-            let library = config.library.to_string();
-            let store = subdomains[i].clone();           
+        for i in 0..config.get_num_domains() {         
             let handle: thread::JoinHandle<_> = thread::spawn(move || {
-                enumerator::query_database(domain.clone(), store.clone(), config.limit);
-                library_enumerator::enumerate(domain, library, store);
+                query.query_database(i, results.store);
+                query.enumerate_library(i, results.store);
             });
 
             threads.push(handle);
         }
     }
     else {
-        for i in 0..config.num_domains {
-            let domain = config.domains[i].to_string();
-            let store = subdomains[i].clone();
+        for i in 0..config.get_num_domains() {
+
             let handle: thread::JoinHandle<_> = thread::spawn(move || {
-                enumerator::query_database(domain.clone(), store.clone(), config.limit);
+                query.query_database(i, results.store);
             });
 
             threads.push(handle);
