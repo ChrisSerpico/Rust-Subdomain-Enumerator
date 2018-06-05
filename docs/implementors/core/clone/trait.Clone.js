68d4c2d0cfac6e12a0a701cde76f94afb506744f
Chris Serpico <christopherserpico2018@u.northwestern.edu>
2018-06-05 17:24:40 -0500

    added documentation to the right folder for pages

---
+++ docs/implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["subdomain_enumerator"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"subdomain_enumerator/query/struct.Query.html\" title=\"struct subdomain_enumerator::query::Query\">Query</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"subdomain_enumerator/results/struct.Results.html\" title=\"struct subdomain_enumerator::results::Results\">Results</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
