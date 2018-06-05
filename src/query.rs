6ba5d9b8adc919cfe3435c83e19d6aab098f8d56
Chris Serpico <christopherserpico2018@u.northwestern.edu>
2018-06-05 12:38:53 -0500

    readded query documentation
    lol

--- src/query.rs
+++ src/query.rs
@@ -6,6 +6,7 @@ use library_enumerator;
 use results::Results;
 use self::threadpool::ThreadPool;
 
+/// Represents a user supplied query, where domains is the list of domains that will have their subdomains enumerated and library is a wordlist supplied for library enumeration.
 #[derive(Debug, Clone)]
 pub struct Query {
     domains:        Vec<String>,
@@ -24,11 +25,13 @@ impl Query {
         }
     }
 
+    /// Add a domain (as a string) to be enumerated. 
     pub fn add_domain(&mut self, domain: String){
         self.domains.push(domain);
         self.num_domains += 1;
     }
 
+    /// Add multiple domains (as a vector of strings) to be enumerated. 
     pub fn add_domains(&mut self, domains: Vec<String>){
         self.domains = domains;
         self.num_domains = self.domains.len();
@@ -42,6 +45,28 @@ impl Query {
         self.limit = limit;
     }
 
+    
+    /// Performs subdomain enumeration on all domains held in the domains variable. The found subdomains will be returned collectively in a single Results object. If a library is supplied, then both library enumeration and a database query will be performed. If no library is supplied, then only a database query will be performed. 
+    /// 
+    /// # Examples
+    ///
+    /// Performing subdomain enumeration on a query with no library, and therefore no library enumeration. 
+    /// '''
+    /// let mut q = Query::new(); 
+    /// q.add_domains(vec!["facebook.com", "google.com"]; 
+    /// let subdomains = q.enumerate(); 
+    /// subdomains.print_subdomains(); 
+    /// ''' 
+    /// 
+    /// Performing subdomain enumeration on a query with a library added. This means that library enumeration will be performed along with normal database querying.
+    /// Note that a library is passed as a string holding a path to an external file. 
+    /// '''
+    /// let mut q = Query::new();
+    /// q.add_domains(vec!["facebook.com", "google.com"]; 
+    /// q.set_library("path_to_library.txt"); 
+    /// let subdomains = q.enumerate(); 
+    /// subdomains.print_subdomains(); 
+    /// ''' 
     pub fn enumerate(&self) -> Results{
         let results = Results::new(self.num_domains, self.domains.clone());
         let pool = ThreadPool::new(4);
